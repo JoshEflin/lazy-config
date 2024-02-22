ed6f8d89969b7cf5ff1c1676e4d92e17653d86ba
ragingpenguin <joshuahuntereflin@gmail.com>
2024-02-22 13:23:15 -0500

    adding luasnip, removing relics

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -27,20 +27,20 @@ require('mason-lspconfig').setup({
   }
 })
 
-local cmp = require('cmp')
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-cmp.setup({
-  sources = {
-    { name = 'path' },
-    { name = 'nvim_lsp' },
-    { name = 'nvim_lua' },
-  },
-  formatting = lsp_zero.cmp_format(),
-  mapping = cmp.mapping.preset.insert({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-Space>'] = cmp.mapping.complete(),
-  }),
-})
+--local cmp = require('cmp')
+--local cmp_select = { behavior = cmp.SelectBehavior.Select }
+--
+--cmp.setup({
+--  sources = {
+--    { name = 'path' },
+--    { name = 'nvim_lsp' },
+--    { name = 'nvim_lua' },
+--  },
+--  formatting = lsp_zero.cmp_format(),
+--  mapping = cmp.mapping.preset.insert({
+--    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+--    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+--    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+--    ['<C-Space>'] = cmp.mapping.complete(),
+--  }),
+--})
---
+++ after/plugin/luasnip.lua
@@ -0,0 +1,49 @@
+-- standard var names for luasnip lib
+-- --
+--local sn = ls.snippet_node
+--local isn = ls.indent_snippet_node
+--local i = ls.insert_node
+--local r = ls.restore_node
+--local events = require("luasnip.util.events")
+--local ai = require("luasnip.nodes.absolute_indexer")
+--local extras = require("luasnip.extras")
+--local l = extras.lambda
+--local rep = extras.rep
+--local p = extras.partial
+--local m = extras.match
+--local n = extras.nonempty
+--local dl = extras.dynamic_lambda
+--local fmt = require("luasnip.extras.fmt").fmt
+--local fmta = require("luasnip.extras.fmt").fmta
+--local conds = require("luasnip.extras.expand_conditions")
+--local postfix = require("luasnip.extras.postfix").postfix
+--local parse = require("luasnip.util.parser").parse_snippet
+--local ms = ls.multi_snippet
+--local k = require("luasnip.nodes.key_indexer").new_key
+
+if not pcall(require, "luasnip") then
+  return
+end
+
+local ls = require("luasnip")
+
+local types = require("luasnip.util.types")
+
+ls.config.set_config {
+  history = true,
+  updateevents = "TextChanged, TextChangedI",
+  enable_autosnippters = true,
+  ext_opts = {
+    [types.choiceNode] = {
+      active = {
+        virt_text = { { "<-- ", "Error" } },
+      },
+    },
+  },
+}
+
+local s = ls.snippet
+local t = ls.text_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local f = ls.function_node
