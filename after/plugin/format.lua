71fa69115ca2a9600eefd8c690d25595565836b8
ragingpenguin <joshuahuntereflin@gmail.com>
2024-06-01 11:46:54 -0400

    fixed deprecated

--- after/plugin/format.lua
+++ after/plugin/format.lua
@@ -2,41 +2,41 @@ local format_on_save = require("format-on-save")
 local formatters = require("format-on-save.formatters")
 
 format_on_save.setup({
-  exclude_path_patterns = {
-    "/node_modules/",
-    ".local/share/nvim/lazy",
-  },
-  formatter_by_ft = {
-    css = formatters.lsp,
-    html = formatters.lsp,
-    java = formatters.lsp,
-    javascript = formatters.lsp,
-    json = formatters.lsp,
-    lua = formatters.lsp,
-    markdown = formatters.prettierd,
-    openscad = formatters.lsp,
-    python = formatters.black,
-    rust = formatters.lsp,
-    scad = formatters.lsp,
-    scss = formatters.lsp,
-    sh = formatters.shfmt,
-    terraform = formatters.lsp,
-    typescript = formatters.lsp,
-    typescriptreact = formatters.lsp,
-    yaml = formatters.lsp,
+    exclude_path_patterns = {
+        "/node_modules/",
+        ".local/share/nvim/lazy",
+    },
+    formatter_by_ft = {
+        css = formatters.lsp,
+        html = formatters.lsp,
+        java = formatters.lsp,
+        javascript = formatters.lsp,
+        json = formatters.lsp,
+        lua = formatters.lsp,
+        markdown = formatters.lsp,
+        openscad = formatters.lsp,
+        python = formatters.black,
+        rust = formatters.lsp,
+        scad = formatters.lsp,
+        scss = formatters.lsp,
+        sh = formatters.shfmt,
+        terraform = formatters.lsp,
+        typescript = formatters.lsp,
+        typescriptreact = formatters.lsp,
+        yaml = formatters.lsp,
 
-    -- Add your own shell formatters:
-    --    myfiletype = formatters.shell({ cmd = { "myformatter", "%" } }),
+        -- Add your own shell formatters:
+        --    myfiletype = formatters.shell({ cmd = { "myformatter", "%" } }),
 
-    -- Add lazy formatter that will only run when formatting:
-    my_custom_formatter = function()
-      if vim.api.nvim_buf_get_name(0):match("/README.md$") then
-        return formatters.prettierd
-      else
-        return formatters.lsp()
-      end
-    end,
+        -- Add lazy formatter that will only run when formatting:
+        my_custom_formatter = function()
+            if vim.api.nvim_buf_get_name(0):match("/README.md$") then
+                return formatters.lsp()
+            else
+                return formatters.lsp()
+            end
+        end,
 
 
-  }
+    }
 })
--- lua/ragingpenguin/plugins.lua
+++ lua/ragingpenguin/plugins.lua
@@ -1,53 +1,85 @@
 return {
-  {
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    options = {},
-  },
+    {
+        "folke/trouble.nvim",
+        opts = {}, -- for default options, refer to the configuration section for custom setup.
+        cmd = "Trouble",
+        keys = {
+            {
+                "<leader>xx",
+                "<cmd>Trouble diagnostics toggle<cr>",
+                desc = "Diagnostics (Trouble)",
+            },
+            {
+                "<leader>xX",
+                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+                desc = "Buffer Diagnostics (Trouble)",
+            },
+            {
+                "<leader>cs",
+                "<cmd>Trouble symbols toggle focus=false<cr>",
+                desc = "Symbols (Trouble)",
+            },
+            {
+                "<leader>cl",
+                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+                desc = "LSP Definitions / references / ... (Trouble)",
+            },
+            {
+                "<leader>xL",
+                "<cmd>Trouble loclist toggle<cr>",
+                desc = "Location List (Trouble)",
+            },
+            {
+                "<leader>xQ",
+                "<cmd>Trouble qflist toggle<cr>",
+                desc = "Quickfix List (Trouble)",
+            },
+        },
+    },
 
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.2',
-    dependencies = { { 'nvim-lua/plenary.nvim' } }
-  },
-  {
-    'folke/tokyonight.nvim',
-    name = 'tokyonight',
-    config = function()
-      vim.cmd('colorscheme tokyonight')
-    end
-  },
-  { 'laytan/cloak.nvim' },
-  { 'lukas-reineke/lsp-format.nvim' },
-  { 'nvim-treesitter/nvim-treesitter' },
-  { 'nvim-treesitter/playground' },
-  {
-    'theprimeagen/harpoon',
-    branch = 'harpoon2',
-    requires = { 'nvim-lua/plenary.nvim' },
-  },
-  { 'mbbill/undotree' },
-  { 'tpope/vim-fugitive' },
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
-    dependencies = {
-      { 'neovim/nvim-lspconfig' },             -- Required
-      { 'williamboman/mason.nvim' },           -- Optional
-      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
-      { 'hrsh7th/nvim-cmp' },                  -- Required
-      { 'hrsh7th/cmp-buffer' },
-      { 'hrsh7th/cmp-path' },
-      { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-      { 'hrsh7th/cmp-nvim-lua' }, -- Required
-      { 'L3MON4D3/LuaSnip' },     -- Required
-      { 'saadparwaiz1/cmp_luasnip' },
-      { 'rafamadriz/friendly-snippets' },
-      { 'elentok/format-on-save.nvim' },
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.2',
+        dependencies = { { 'nvim-lua/plenary.nvim' } }
+    },
+    {
+        'folke/tokyonight.nvim',
+        name = 'tokyonight',
+        config = function()
+            vim.cmd('colorscheme tokyonight')
+        end
+    },
+    { 'laytan/cloak.nvim' },
+    { 'lukas-reineke/lsp-format.nvim' },
+    { 'nvim-treesitter/nvim-treesitter' },
+    { 'nvim-treesitter/playground' },
+    {
+        'theprimeagen/harpoon',
+        branch = 'harpoon2',
+        requires = { 'nvim-lua/plenary.nvim' },
+    },
+    { 'mbbill/undotree' },
+    { 'tpope/vim-fugitive' },
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        dependencies = {
+            { 'neovim/nvim-lspconfig' },             -- Required
+            { 'williamboman/mason.nvim' },           -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+            { 'hrsh7th/nvim-cmp' },                  -- Required
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'hrsh7th/cmp-nvim-lua' }, -- Required
+            { 'L3MON4D3/LuaSnip' },     -- Required
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'rafamadriz/friendly-snippets' },
+            { 'elentok/format-on-save.nvim' },
+        }
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
     }
-  },
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' }
-  }
 }
