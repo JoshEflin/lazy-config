ea04d578cc30e9512913cd8123c0384cf3f133d2
ragingpenguin <joshuahuntereflin@gmail.com>
2024-12-20 07:58:41 -0500

    leader vf

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,11 @@
-
 local builtin = require('telescope.builtin')
 
 -- Define key mappings using vim.api.nvim_set_keymap
-vim.api.nvim_set_keymap('n', '<leader>pf', ':lua require("telescope.builtin").find_files()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-p>', ':lua require("telescope.builtin").git_files()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>ps', ':lua require("telescope.builtin").grep_string({search=vim.fn.input("Grep > ")})<CR>', { noremap = true, silent = true })
-
+vim.keymap.set('n', '<leader>pf', function() builtin.find_files() end)
+vim.keymap.set('n', '<leader>vf', function()
+    builtin.find_files {
+        cwd = vim.fn.stdpath("config")
+    }
+end)
+vim.keymap.set('n', '<C-p>', require("telescope.builtin").git_files)
+vim.keymap.set('n', '<leader>ps', function() builtin.grep_string({ search = vim.fn.input("Grep > ") }) end)
